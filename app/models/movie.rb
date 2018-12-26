b55735ed68d84093ec2f2d0b55ff9b8af5e59039
Ramyasri <ramyasri.a19@gmail.com>
2018-12-26 22:15:55 +0530

    Updated migrations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ApplicationRecord
   has_many :reviews
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :text
+
+      t.timestamps
+    end
+  end
 end
